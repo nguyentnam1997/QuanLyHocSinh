69e8ca2c0039830660286e66688dc3ce3c7e237a
nguyentnam1997 <nguyentnam.061297@gmail.com>
2023-08-04 17:36:05 +0700

    Quản lý học sinh

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        StudentMethods ultilities = new StudentMethods();
         int countId = Student.getId(); //tạo biến count để set ID tăng dần
         do {
             //Nhập số học sinh + tạo mảng chứa học sinh
@@ -15,11 +16,13 @@ public class Main {
             Student[] arrStudent = new Student[n];  //Tạo mảng lưu danh sách các học sinh
             for (int i = 0; i < arrStudent.length; i++) {
                 System.out.println("----------SINH VIÊN THỨ " + (i + 1) + "----------");
-                StudentMethods s = new StudentMethods();
-                arrStudent[i] = s.CreateStudent(scanner, countId);
+                //StudentMethods s = new StudentMethods();
+                arrStudent[i] = ultilities.CreateStudent(scanner, countId);
                 countId++;  //Sau khi tạo và gán Học sinh vào mảng, count tăng 1 cho id
             }
-            //////Menu các chứng năng
+            //////Menu các chức năng
+
+
             for (int i = 0; i < arrStudent.length; i++) {
                 System.out.println(arrStudent[i].Show());
             }
--- src/StudentMethods.java
+++ src/StudentMethods.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class StudentMethods {
+    //Phương thức tạo Học sinh
     public Student CreateStudent(Scanner scanner, int id) {
         System.out.println("Mời nhập tên:");
         String name = scanner.nextLine();
@@ -14,6 +15,17 @@ public class StudentMethods {
         String classroom = scanner.nextLine();
         return new Student(id, name, age, address, point, classroom);
     }
-
+     //Phương thức hiện Menu
+    public void ShowMenu() {
+        System.out.println("----------MENU-----------");
+        System.out.println("1. Xem danh sách học sinh");
+        System.out.println("2. Thêm học sinh mới");
+        System.out.println("3. Cập nhật điểm học sinh");
+        System.out.println("4. Xoá học sinh");
+        System.out.println("5. Xem danh sách học sinh theo lớp");
+        System.out.println("6. Sắp xếp theo tên");
+        System.out.println("7. Sắp xếp theo tuổi");
+        System.out.println("8. Sắp xếp theo điểm");
+    }
 
 }
